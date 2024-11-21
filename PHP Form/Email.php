5c167cc5f135d22542825b93dca07f0d576eaee6
ShahriarSaikat9903 <151661216+ShahriarSaikat9903@users.noreply.github.com>
2024-11-21 16:21:21 +0600

    Add files via upload

---
+++ PHP Form/Email.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['email_submit'])) {
+
+    $email = $_POST['email'];
+
+    if (empty($email)) 
+    {
+
+        echo "Email cannot be empty.";
+
+    } 
+
+    elseif (strpos($email, '@') === false || strpos($email, '.') === false || strpos($email, '@') > strrpos($email, '.')) 
+    {
+
+        echo "Invalid email format.";
+
+    }
+
+    else 
+    {
+        echo "Email is valid.";
+    }
+
+}
+
+
+?>
